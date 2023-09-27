d8f8ae5fcf3fc9f5055a7fd6b49e21a01aef0a5b
Francisco Santos <frankluzon@gmail.com>
2023-09-27 12:32:20 -0300

    Update cartaoProduto.js
    Imagens não estavam aparecendo

--- src/cartaoProduto.js
+++ src/cartaoProduto.js
@@ -6,7 +6,7 @@ export function renderizarCatalogo(params) {
     for (const produto of catalogo) {
         const cartaoProduto = `
         <div class="border-solid shadow-xl shadow-slate-400 rounded-lg w-48 m-2 flex flex-col p-2 justify-between group ${produto.pretreino ? 'pretreino': 'postreino'} " id="card-produto-${produto.id}">
-            <img src="./assets/img/${produto.nomeArquivoImagem}" 
+            <img src="/assets/img/${produto.nomeArquivoImagem}" 
             alt="Produto 1 do Magazine Hahtag." class="group-hover:scale-110 duration-300 my-3 rounded-lg" />
             <p class='text-sm'>${produto.nome}</p>
             <p class='text-sm'>${produto.descricao}</p>
